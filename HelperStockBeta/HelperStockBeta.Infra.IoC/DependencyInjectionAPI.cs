780aeac5a5f10ad653913687f00a81ffd4de3122
GuhRodri <gustavo_rodrigues_silva@hotmail.com>
2023-12-04 12:00:29 -0300

    Aula0412 - Swagger e API Core referencia prova

---
+++ HelperStockBeta/HelperStockBeta.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,37 @@
+﻿using HelperStockBeta.Application.Interfaces;
+using HelperStockBeta.Application.Mappings;
+using HelperStockBeta.Application.Services;
+using HelperStockBeta.Domain.Interface;
+using HelperStockBeta.Infra.Data.Context;
+using HelperStockBeta.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            //Mapping Injection DTOs
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
